553689f65a01469de73c785ca4f3beacc416d07f
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-04-15 10:47:41 +0900

    add switchNetwork method to useWallet

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,23 @@
+import { useEffect } from "react";
+import { useWallet } from "../hook/useWallet";
 import ConnectButton from "./ConnectButton";
 
 const Navbar = () => {
+  const { currentAccount } = useWallet();
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">DrumSwap</a>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <ConnectButton />
-          </li>
-        </ul>
+        {currentAccount && (
+          <button className="btn btn-ghost mx-2">
+            {currentAccount?.slice(0, 6) + "..." + currentAccount?.slice(-4)}
+          </button>
+        )}
+
+        {!currentAccount && <ConnectButton className="mx-2" />}
       </div>
     </div>
   );
