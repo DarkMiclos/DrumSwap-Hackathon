e71bf9ee5517bf71fcfd8db7d8188fa0ae97fb08
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-04-15 22:00:21 +0900

    Get data from input for the smart contracts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { useWallet, isWalletConnected } from "./hook/useWallet";
-import { useDrumFactoryContract } from "./hook/useDrumSwap";
-import ConnectButton from "./components/ConnectButton";
 import Swap from "./components/Swap";
 import Pool from "./pages/Pool";
 import Navbar from "./components/Navbar";
 
 const App = () => {
-  const { currentAccount } = useWallet();
-  const { swap, getPair, createPair } = useDrumFactoryContract({
-    currentAccount,
-  });
-
   return (
     <>
       <Navbar />
--- frontend/src/pages/Pool.tsx
+++ frontend/src/pages/Pool.tsx
@@ -1,114 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
 import { BigNumber, ethers } from "ethers";
 import routerContractABI from "../abi/router.json";
-import usdtAbi from "../abi/usdt.json";
 import { swapFromTokenState, swapToTokenState } from "../utils/atom";
 import TokenSelectionButton from "../components/TokenSelectionButton";
 import { useRecoilValue } from "recoil";
 import { useWallet } from "../hook/useWallet";
 
-const routerContractAdress = "0x46098Af079142A0D03e224597E74C758f6c6B2d3";
-const usdtTokenAdress = "0x9b75777bbb43ce39f80B33eEeaCb54141f90c4f8";
-const drumTokenAdress = "0xfC605CB680AfDf4FA4B2222010668013929a3F3F";
+const routerContractAddress = "0x46098Af079142A0D03e224597E74C758f6c6B2d3";
+const usdtTokenAddress = "0x9b75777bbb43ce39f80B33eEeaCb54141f90c4f8";
+const drumTokenAddress = "0xfC605CB680AfDf4FA4B2222010668013929a3F3F";
 
 const Pool = () => {
-    const swapFromToken = useRecoilValue(swapFromTokenState);
-    const swapToToken = useRecoilValue(swapToTokenState);
-    const usdtAmount = ethers.utils.parseEther("1000");
-    const drumAmount = ethers.utils.parseEther("1000");
-    const { currentAccount } = useWallet();
-    let signer: any;
+  const swapFromToken = useRecoilValue(swapFromTokenState);
+  const swapToToken = useRecoilValue(swapToTokenState);
+  const { currentAccount } = useWallet();
+  const [fromTokenAmount, setFromTokenAmount] = useState(0);
+  const [toTokenAmount, setToTokenAmount] = useState(0);
 
-    let getRouterContract = () => {
-        if(window.ethereum) {
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            signer = provider.getSigner();
-            let contract = new ethers.Contract(
-            routerContractAdress,
-            routerContractABI,
-            signer
-            );
-            return contract;
-        }
+  let signer: any;
+
+  let getRouterContract = () => {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      signer = provider.getSigner();
+      let contract = new ethers.Contract(
+        routerContractAddress,
+        routerContractABI,
+        signer
+      );
+      return contract;
     }
+  };
 
-    let getUsdtContract = () => {
-        if(window.ethereum) {
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            signer = provider.getSigner();
-            let contract = new ethers.Contract(
-            usdtTokenAdress,
-            usdtAbi,
-            signer
-            );
-            return contract;
-        }
-    }
+  const handleChangeFromAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFromTokenAmount(Number(e.currentTarget.value));
+  };
 
-    let getDrumContract = () => {
-        if(window.ethereum) {
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            signer = provider.getSigner();
-            let contract = new ethers.Contract(
-            drumTokenAdress,
-            usdtAbi,
-            signer
-            );
-            return contract;
-        }
-    }
+  const handleChangeToAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setToTokenAmount(Number(e.currentTarget.value));
+  };
 
-    let addLiquidity = async (tokenAdressA: string | undefined, tokenAdressB: string | undefined, amountADesired: BigNumber | undefined,
-         amountBDesired: BigNumber | undefined, amountAMin: number | undefined,
-        amountBMin: number | undefined, to: string | undefined,  deadline: any, gasLimit: any ) => {
-        /*await getUsdtContract()?.approve(getRouterContract()?.address, usdtAmount,{
-            gasLimit: 5900000
-            });
-        await getDrumContract()?.approve(getRouterContract()?.address, drumAmount,{
-        gasLimit: 5900000
-        });*/
-        getRouterContract()?.addLiquidity(tokenAdressA, tokenAdressB, amountADesired, amountBDesired, amountAMin, amountBMin, to, deadline, gasLimit)
-        .then(() => console.log())
-    ;
-    }
+  let addLiquidity = async (
+    tokenAddressA: string | undefined,
+    tokenAddressB: string | undefined,
+    amountADesired: BigNumber | undefined,
+    amountBDesired: BigNumber | undefined,
+    amountAMin: number | undefined,
+    amountBMin: number | undefined,
+    to: string | undefined,
+    deadline: any,
+    gasLimit: any
+  ) => {
+    getRouterContract()
+      ?.addLiquidity(
+        tokenAddressA,
+        tokenAddressB,
+        amountADesired,
+        amountBDesired,
+        amountAMin,
+        amountBMin,
+        to,
+        deadline,
+        gasLimit
+      )
+      .then(() => console.log());
+  };
 
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="card w-[60vw] w-min-[60vw] shadow-xl">
-            <div className="card-body">
-                <h2 className="card-title justify-center">Add Liquidity</h2>
-                <div className="divider"></div>
-                <label className="input-group flex justify-center">
-                    <input type="text" placeholder="0.01" className="input input-bordered" />
-                    <TokenSelectionButton
-                        type="from"
-                        className="select select-bordered rounded-sm"
-                    >
-                        {swapFromToken}
-                    </TokenSelectionButton>
-                </label>
-                <label className="input-group flex justify-center">
-                    <input type="text" placeholder="0.01" className="input input-bordered" />
-                    <TokenSelectionButton
-                        type="from"
-                        className="select select-bordered rounded-sm"
-                    >
-                        {swapToToken}
-                    </TokenSelectionButton>
-                </label>
-                <div className="divider"></div>
-                <div className="card-actions justify-center">
-                    <button className="btn btn-primary" onClick={() => addLiquidity(usdtTokenAdress, 
-                        drumTokenAdress, usdtAmount, drumAmount, 0, 0, currentAccount, Math.floor(Date.now() / 1000) + 60 * 10,
-                        {
-                            gasLimit: 4000000
-                        }
-                        )}>Add Liquidity</button>
-                </div>
-            </div>
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="card w-[60vw] w-min-[60vw] shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Add Liquidity</h2>
+          <div className="divider"></div>
+          <label className="input-group flex justify-center">
+            <input
+              onChange={handleChangeFromAmount}
+              type="text"
+              placeholder="0.01"
+              className="input input-bordered"
+            />
+            <TokenSelectionButton
+              type="from"
+              className="select select-bordered rounded-sm"
+            >
+              {swapFromToken}
+            </TokenSelectionButton>
+          </label>
+          <label className="input-group flex justify-center">
+            <input
+              onChange={handleChangeToAmount}
+              type="text"
+              placeholder="0.01"
+              className="input input-bordered"
+            />
+            <TokenSelectionButton
+              type="to"
+              className="select select-bordered rounded-sm"
+            >
+              {swapToToken}
+            </TokenSelectionButton>
+          </label>
+          <div className="divider"></div>
+          <div className="card-actions justify-center">
+            <button
+              className="btn btn-primary"
+              onClick={() =>
+                addLiquidity(
+                  usdtTokenAddress,
+                  drumTokenAddress,
+                  ethers.utils.parseEther(fromTokenAmount.toString()),
+                  ethers.utils.parseEther(toTokenAmount.toString()),
+                  0,
+                  0,
+                  currentAccount,
+                  Math.floor(Date.now() / 1000) + 60 * 10,
+                  {
+                    gasLimit: 4000000,
+                  }
+                )
+              }
+            >
+              Add Liquidity
+            </button>
+          </div>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Pool;
